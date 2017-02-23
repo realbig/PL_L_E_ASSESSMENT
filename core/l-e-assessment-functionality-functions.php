1ec33248cc2583373320629fe35fc32539ef2631
Eric Defore <eric.defore@gmail.com>
2017-02-23 11:18:23 -0500

    v0.1.0

--- core/l-e-assessment-functionality-functions.php
+++ core/l-e-assessment-functionality-functions.php
@@ -10,9 +10,7 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	die;
 }
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 /**
  * Returns the main plugin object
  *
